f1db1af9b05c8a5f6fe59894db29a89dda1c5019
Brian Mwangi <britabyzk29@gmail.com>
2023-09-07 15:37:08 +0300

    change inteface

--- controllers/disposedImagecontroller.js
+++ controllers/disposedImagecontroller.js
@@ -1,33 +1,39 @@
 const asyncHandler = require('express-async-handler');
 const DisposedProd = require('../models/Disposedwastemodel');
 
+const DisposeItem = asyncHandler(async (req, res) => {
+    const { imageLink, quantity, description } = req.body;
 
-const DisposeItem = asyncHandler(async(req,res)=>{
-    const {imageLink,quantity,description} = req.body;
-    if (!imageLink || !quantity || !description) {
-        res.status(400);
-        throw new Error("All fields are mandatory");
+    if (!imageLink) {
+        return res.status(400).json({ error: "Image is missing" });
     }
-    try{
-      if(req.user) { 
-        const newProduct =await DisposedProd.create({
-            user_id: req.user.id,
-            imageLink,
-            quantity,
-            description,
-        });
-        // Save the image to the database
-        await newProduct.save();
-        res.status(201).json(newProduct);
-    }else{
-        res.status(401);
-        throw new Error("User is not authorized or token is missing in request");
+    if (!quantity) {
+        return res.status(400).json({ error: "Quantity is missing" });
     }
-    
-    }catch(error){
-        throw new Error("Error Saving disposed Product Details");
+    if (!description) {
+        return res.status(400).json({ error: "Description is missing" });
     }
-    
-})
 
-module.exports = {DisposeItem}
+    try {
+        if (req.user) {
+            const newProduct = await DisposedProd.create({
+                user_id: req.user.id,
+                imageLink,
+                quantity,
+                description,
+            });
+
+            // Save the image to the database
+            await newProduct.save();
+            console.log(req.user.id);
+            return res.status(201).json(newProduct);
+        } else {
+            return res.status(401).json({ error: "User is not authorized or token is missing in the request" });
+        }
+    } catch (error) {
+        console.error("Error saving disposed Product Details:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+module.exports = {DisposeItem};
